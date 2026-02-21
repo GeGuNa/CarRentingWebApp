a664e3cec4e3323d60342a8e430f07a887fc7b5d
GeGuNa <bnukri291@gmail.com>
2026-02-21 07:29:52 +0400

    ..

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'carlink_db';
+$username = 'root'; 
+$password = '';     
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+?>
